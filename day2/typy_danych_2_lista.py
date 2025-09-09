7024fc7ff0e1a92153ac35af3724d7b90a42a978
rajkonkret660@gmail.com <rajkonkret660@gmail.com>
2025-09-09 10:37:17 +0200

    [2 5] update typy_danych_2_lista.py

--- day2/typy_danych_2_lista.py
+++ day2/typy_danych_2_lista.py
@@ -52,5 +52,62 @@ print(lista[2:7])  # bez ostatniego
 
 print(lista[2:20])  # ['Zenek', 'Ola', 'Aga', 'Kamil', 'Adam', 'Bogdan']
 print(lista[12:56])  # []
-print(lista[2:2]) # []
-print(lista[2:3]) # ['Zenek']
+print(lista[2:2])  # []
+print(lista[2:3])  # ['Zenek']
+
+print(lista[:])
+# ['Radek', 'Tomek', 'Zenek', 'Ola', 'Aga', 'Kamil', 'Adam', 'Bogdan']
+
+# ['Radek', 'Tomek', 'Zenek', 'Ola', 'Aga', 'Kamil', 'Adam', 'Bogdan']
+#     0         1       2       3      4       5        6        7
+#     -8        -7      -6      -5     -4      -3      -2        -1
+print(lista[-2:0])  # -> [6:0] -> []
+print(lista[0:-2])  # 0:6 -> ['Radek', 'Tomek', 'Zenek', 'Ola', 'Aga', 'Kamil']
+
+# range() - generowanie z zakresu
+lista_15 = list(range(15))  # od 0 do 14
+print(lista_15)  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+print(lista_15[0:15:2])  # [start:stop:krok] [0, 2, 4, 6, 8, 10, 12, 14] ostatni niewłącznie
+
+print(list(range(0, 15, 2)))  # (start, stop, krok), [0, 2, 4, 6, 8, 10, 12, 14]
+
+print(lista[::2])  # cała lista, co 2, ['Radek', 'Zenek', 'Aga', 'Adam']
+
+print(lista[::-1])  # odwrócona lista
+# ['Bogdan', 'Adam', 'Kamil', 'Aga', 'Ola', 'Zenek', 'Tomek', 'Radek']
+
+# nadpisanie elementu w liście na wskazanym indeksie
+# zmiana na oryginalnej liście
+lista[3] = "Asia"
+print(lista)
+# ['Radek', 'Tomek', 'Zenek', 'Asia', 'Aga', 'Kamil', 'Adam', 'Bogdan']
+
+# wstawienie elementu na wskazanym indeksie
+lista.insert(1, "Ola")
+print(lista)
+# ['Radek', 'Ola', 'Tomek', 'Zenek', 'Asia', 'Aga', 'Kamil', 'Adam', 'Bogdan']
+
+# usunięcie elementu z listy, po elemencie
+lista.remove("Tomek")
+print(lista)
+# ['Radek', 'Ola', 'Zenek', 'Asia', 'Aga', 'Kamil', 'Adam', 'Bogdan']
+
+# usunięci epo indeksie
+# pop()
+print(lista.pop(2))
+# Zenek
+zmienna = lista.pop(-1)
+print("Zmienna:", zmienna)  # Zmienna: Bogdan
+print(lista)
+
+# ['Radek', 'Ola', 'Asia', 'Aga', 'Kamil', 'Adam']
+lista.append("Radek")
+print(lista)
+lista.remove("Radek")  # usunie pierwszy napotkany
+print(lista)
+# ['Ola', 'Asia', 'Aga', 'Kamil', 'Adam', 'Radek']
+
+# sprawdzenie indexu dla danego eleemntu
+
+print(lista.index("Asia"))  # index 1
