f7c1ae619c8cd59d5be4083853814efca76c650b
rajkonkret660@gmail.com <rajkonkret660@gmail.com>
2025-09-09 16:02:39 +0200

    [2 12a] update warnki

--- day2/warunki.py
+++ day2/warunki.py
@@ -50,17 +50,85 @@ if n > 3:
 if (n := len(a)) > 3:
     print(f"Długość wynosi {n}, więcej niż 3")
 
-podatek = 0
-zarobki = int(input("Podaj zarobki"))
+# podatek = 0
+# zarobki = int(input("Podaj zarobki"))
+#
+# if zarobki < 10_000:
+#     podatek = 0
+# elif zarobki < 40_000:
+#     podatek = 0.2
+# elif zarobki < 100_000:
+#     podatek = 0.4
+# else:
+#     podatek = 0.9
+#
+# print(f"Podatek wynosi {zarobki * podatek} pln")
+# # podatek 0.2 od 10_000 do 39_999
+# ctrl /  - komentowanie
 
-if zarobki < 10_000:
-    podatek = 0
-elif zarobki < 40_000:
-    podatek = 0.2
-elif zarobki < 100_000:
-    podatek = 0.4
+sum_zam = 150
+if sum_zam > 100:
+    rabat = 25
 else:
-    podatek = 0.9
+    rabat = 0
+print(f"Rabat wynosi {rabat}")  # Rabat wynosi 25
 
-print(f"Podatek wynosi {zarobki * podatek} pln")
-# podatek 0.2 od 10_000 do 39_999
+rabat = 25 if sum_zam > 100 else 0
+print(f"Rabat wynosi {rabat}")  # Rabat wynosi 25
+
+# zasymulujemy system zbierania logów
+# zmienna przechowujący typ systemu
+# console, email, inny
+# console: "Stało się coś strasznego"
+# email: "System email"
+# jesli system email
+# do listy błedów dodac tłumaczenie błedu
+# zmienna przechowuje poziom błedu
+# error, medium, inny
+alert_system = "email"
+error = "error"
+lista_b = []
+if alert_system == "console":
+    print("Stało się coś strasznego")
+elif alert_system == "email":
+    print("System email")
+    if error == "error":
+        lista_b.append("Krytyczny")
+    elif error == "medium":
+        lista_b.append("Ostrzeżenie")
+    else:
+        lista_b.append("Inny")
+
+else:
+    print("Inny system")
+
+print(lista_b)
+# System email
+# ['Krytyczny']
+
+# napisać test z....
+# 3 pytania
+# dodac punktację
+punkty = 0
+odp = input("Podaj rok Chrztu Polski")
+if odp.strip() == '966':
+    print("Prawidłowa odpowiedź")
+    # punkty = punkty + 1
+    punkty += 1
+else:
+    print("Błędna odpowiedź")
+print(punkty)
+
+odp = input("Podaj rok Chrztu Polski")
+if odp.strip() == '966':
+    print("Prawidłowa odpowiedź")
+    punkty += 1
+else:
+    print("Błędna odpowiedź")
+
+print(punkty)
+# spam += 1    spam = spam + 1
+# spam -= 1    spam = spam - 1
+# spam *= 1    spam = spam * 1
+# spam /= 1    spam = spam / 1
+# spam %= 1    spam = spam % 1
