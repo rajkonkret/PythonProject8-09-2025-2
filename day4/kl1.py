d516b40679607b41da134d905e589611de22dcb6
rajkonkret660@gmail.com <rajkonkret660@gmail.com>
2025-09-11 13:59:22 +0200

    [4 10] add kl1.py

---
+++ day4/kl1.py
@@ -0,0 +1,22 @@
+# klasa  - element programowania obiektowego
+# klasa - przepis, szablon
+# cechy (zmienne)
+# metody  - funkcje w klasie
+# obiekt - instancja klasy
+# klasa musi zostac zadekalrowana przed użyciem
+# # tworzenie obiektu klasy uruchumia funkcję inicjalizującą (konstruktor) __init__
+# paradygmaty -> hermetyzacja, dziedziczenie, polimorfizm, abstrakcja
+
+# PascalCase, UpperCamelCase
+class Human:
+    """
+    Klasa Human opisująca człowieka w pythonie
+    """
+
+
+print(Human.__doc__)
+# dokumentacja klasy
+# Klasa Human opisująca człowieka w pythonie
+# cd day4 - przejście do katalogu day4
+# pydoc -b - serwer dokumentacyjny
+# pydoc -w kl1
