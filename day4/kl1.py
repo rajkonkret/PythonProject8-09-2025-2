ee6a7ecbbf0eb662aed61d7f273f2cd6207cebf3
rajkonkret660@gmail.com <rajkonkret660@gmail.com>
2025-09-11 14:16:53 +0200

    [4 11] update kl1.py

--- day4/kl1.py
+++ day4/kl1.py
@@ -13,6 +13,14 @@ class Human:
     Klasa Human opisująca człowieka w pythonie
     """
 
+    imie = ""
+    wiek = None
+    plec = "k"
+
+    def powitanie(self):
+        print(f"Nazywam się: {self.imie}")
+        # print(f"Nazywam się: {cz1.imie}")
+
 
 print(Human.__doc__)
 # dokumentacja klasy
@@ -20,3 +28,38 @@ print(Human.__doc__)
 # cd day4 - przejście do katalogu day4
 # pydoc -b - serwer dokumentacyjny
 # pydoc -w kl1
+
+# tworzenie obiektu klasy
+cz1 = Human()
+print(cz1)
+# <__main__.Human object at 0x000002650FE96F90>
+print(cz1.imie)
+print(cz1.wiek)
+print(cz1.plec)
+#
+# None
+# k
+
+cz1.imie = "Anna"
+cz1.wiek = 34
+print(cz1.imie)
+print(cz1.wiek)
+print(cz1.plec)
+# Anna
+# 34
+# k
+
+cz2 = Human()
+cz2.imie = 'Radek'
+cz2.wiek = 67
+cz2.plec = "m"
+print(cz2.imie)
+print(cz2.wiek)
+print(cz2.plec)
+# Radek
+# 67
+# m
+
+cz1.powitanie()
+# Nazywam się: Anna
+cz2.powitanie()  # Nazywam się: Radek
